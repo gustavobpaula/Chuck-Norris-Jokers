959a4f313805140bfaf6642c56ea6a2aedc68782
Gustavo Bento de Paula <gustavob.paula@gmail.com>
2019-12-10 18:28:26 -0300

    feat: :rocket: add typograpjy variant

--- src/components/atoms/Typography/style.js
+++ src/components/atoms/Typography/style.js
@@ -3,28 +3,28 @@ import { pxToRem } from '../../../utils';
 import designSystem from '../../../designSystem';
 
 const variants = {
-  title: () => css`
-    display: block;
-    font-size: ${pxToRem(26)};
-    font-weight: 500;
-    margin-bottom: 20px;
-    margin-top: 80px;
-    text-align: center;
-
-    @media (min-width: ${designSystem.get('display.sizes.desktop')}) {
-      font-size: ${pxToRem(32)};
-    }
-  `,
-
   joke: () => css`
-    font-size: ${pxToRem(18)};
-    line-height: ${pxToRem(26)};
-    margin-bottom: 15px;
-    text-align: center;
+    font-size: ${pxToRem(60)};
+    text-shadow: -1px 0 ${designSystem.get('colors.color3')},
+      0 1px ${designSystem.get('colors.color3')},
+      1px 0 ${designSystem.get('colors.color3')},
+      0 -1px ${designSystem.get('colors.color3')};
+
+    @media (max-width: ${designSystem.get('display.sizes.phone')}) {
+      font-size: ${pxToRem(40)};
+    }
   `,
 
   category: () => css`
     font-size: ${pxToRem(40)};
+
+    @media (max-width: ${designSystem.get('display.sizes.phone')}) {
+      font-size: ${pxToRem(22)};
+    }
+  `,
+
+  button: () => css`
+    font-size: ${pxToRem(30)};
 
     @media (max-width: ${designSystem.get('display.sizes.phone')}) {
       font-size: ${pxToRem(22)};
