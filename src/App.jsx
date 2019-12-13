8ea3c9759005aa9719e19e031ec2a77effcc3f71
Gustavo Bento de Paula <gustavo.bento@lendico.com.br>
2019-12-13 16:33:17 -0300

    fix: :bug: redirect 404

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import { Provider } from 'react-redux';
 import { Switch, Route } from 'react-router-dom';
 import { ConnectedRouter } from 'connected-react-router';
 import Load from 'components/pages/Load';
+import Page404 from 'components/pages/404';
 import store, { history } from './store';
 import GlobalStyle from './globalStyle';
 
 export const Home = lazy(() => import('components/pages/Home'));
 export const Category = lazy(() => import('components/pages/Category'));
-export const Page404 = lazy(() => import('components/pages/404'));
 
 export default () => (
   <>
@@ -19,7 +19,7 @@ export default () => (
           <Suspense fallback={<Load />}>
             <Route exact path="/" component={Home} />
             <Route exact path="/category/:id" component={Category} />
-            <Route path="/404" component={Page404} />
+            <Route component={Page404} />
           </Suspense>
         </Switch>
       </ConnectedRouter>
